4bc2282f9712d80b20b6b8f305d93d6b8224564a
feoh <feoh@feoh.org>
2019-11-08 22:59:56 -0500

    Add data access methods - some of them anyway :)

--- server/db/data_access.py
+++ server/db/data_access.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from db.data_models import ModelBase, Rooms, BlockedRooms, House, Posts
+from datetime import datetime
+
 
 def initialize_orm():
-    engine: Engine = create_engine("sqlite:///myplace-db.sqlite")
-    data_models.ModelBase.metadata.create_all(engine)
+    engine = create_engine("sqlite:///myplace-db.sqlite")
+    ModelBase.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
@@ -13,3 +16,14 @@ def initialize_orm():
 def get_all_rooms(session):
     session = initialize_orm()
     all_rooms_query = session.query()
+
+
+def get_all_posts(session):
+    session = initialize_orm()
+    all_posts_query = session.query()
+
+
+def create_post(session, room, date, title, body):
+    post = Posts(room="LivingRoom", date=datetime.now(), title=title, body=body)
+    session.add(post)
+    session.commit()
--- server/db/data_models.py
+++ server/db/data_models.py
@@ -1,24 +1,23 @@
-from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.orm import relationship
 
 ModelBase = declarative_base()
 
-# The basic unit of communication in MyHouse is the Post.
-class Posts(ModelBase):
-    __tablename__ = "posts"
-    room = relationship(Room)
-    id = Column(Integer, primary_key=True)
-    date = Column(DateTime)
-    subject = Column(String, unique=True)
-    body = Column(String)
-
 
 class Rooms(ModelBase):
     id = Column(Integer, primary_key=True)
     name = Column(String, unique=True)
-    posts = relationship(Posts, backref="posts")
+
+
+# The basic unit of communication in MyHouse is the Post.
+class Posts(ModelBase):
+    __tablename__ = "posts"
+    id = Column(Integer, primary_key=True)
+    room = relationship(Rooms)
+    date = Column(DateTime)
+    title = Column(String, unique=True)
+    body = Column(String)
 
 
 class BlockedRooms(ModelBase):
