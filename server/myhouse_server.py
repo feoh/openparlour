5cb1b79869d97f9d6cacc3978b06ef81dd311220
feoh <notwen25>
2019-12-27 04:26:03 -0500

    Update the diagram with a bad ASCII design blurb :)

--- server/myhouse_server.py
+++ server/myhouse_server.py
@@ -2,6 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/myplace/post/get/<int:post_number>')
+@app.route('/openparlour/post/get/<int:post_number>')
 def get_post(post_number):
     pass
+
+@app.route('/openparlour/post/')
