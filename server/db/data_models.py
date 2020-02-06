c4fd232c72a3c73e84449306f697ab9e5c3b2523
feoh <feoh@feoh.org>
2020-02-05 23:47:51 -0500

    The tests are working! They're not passing in such a way as to inform me that my data layer is hopelessly deranged :) Time to come back with fresh eyes and learn how to fix that :)

--- server/db/data_models.py
+++ server/db/data_models.py
@@ -1,15 +1,16 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 
 ModelBase = declarative_base()
 
 
 class Room(ModelBase):
+    __tablename__ = "rooms"
     id = Column(Integer, primary_key=True)
     name = Column(String, unique=True)
 
-    def to_json(self):
+    def to_dict(self):
         return {
             'id': self.id,
             'name': self.name
@@ -20,12 +21,12 @@ class Room(ModelBase):
 class Post(ModelBase):
     __tablename__ = "posts"
     id = Column(Integer, primary_key=True)
-    room = relationship(Room)
+    room = relationship(Room, uselist=False)
     date = Column(DateTime)
     title = Column(String, unique=True)
     body = Column(String)
 
-    def to_json(self):
+    def to_dict(self):
         return {
             'id': self.id,
             'room': self.room,
@@ -35,27 +36,15 @@ class Post(ModelBase):
         }
 
 
-class BlockedRooms(ModelBase):
-    id = Column(Integer, primary_key=True)
-    rooms = relationship(Room)
-
-    def to_json(self):
-        return {
-            'id': self.id,
-            'rooms': self.rooms
-        }
-
-
 class House(ModelBase):
+    __tablename__ = "houses"
     id = Column(Integer, primary_key=True)
     address = Column(String, primary_key=True)
-    blocked_rooms = relationship(BlockedRooms)
 
-    def to_json(self):
+    def to_dict(self):
         return {
             'id': self.id,
             'address': self.address,
-            'blocked_rooms': self.blocked_rooms
         }
 
 
--- server/test/test_data_models.py
+++ server/test/test_data_models.py
@@ -1,9 +1,10 @@
 from db.data_models import ModelBase, Post, Room, House, BlockedRooms
 import datetime
 def test_post_to_json():
-    r = Room(1, "Den")
-    p = Post(1, r, datetime.datetime.now(), "Test Post", "This is a test of the emergency post system.")
+    r = Room(id=1, name="Den")
+    p = Post(id=1, room=r, date=datetime.datetime(2020,2,1), title="Test Post",
+             body="This is a test of the emergency post system.")
 
-    j = p.to_json()
+    assert p.to_dict() == { 'id':1, 'date': datetime.datetime(2020,2,1), 'title': "Test Post",
+                            'body': "This is a test of the emergency post system."}
 
-    assert(p)
