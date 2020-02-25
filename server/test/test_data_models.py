2a6da7b6bc508716f37fc367de395a5ff0828888
feoh <feoh@feoh.org>
2020-02-25 05:35:13 -0500

    Use Pytest fixtures because they RULE! :)

--- server/test/test_data_models.py
+++ server/test/test_data_models.py
@@ -1,6 +1,14 @@
 from db.data_models import Post, Room, House
-from db.data_access import nuke_db, initialize_orm, create_post
+from db.data_access import nuke_db, initialize_orm, create_post, create_house
+import pytest
 import datetime
+
+
+@pytest.fixture
+def test_db():
+    test_db = initialize_orm("sqlite:///test-openparlour-db.sqlite")
+    yield test_db
+    nuke_db("sqlite:///test-openparlour-db.sqlite")
 
 
 def test_post_to_dict():
@@ -19,12 +27,13 @@ def test_room_to_dict():
 
 
 def test_house_to_dict():
-    h = House(1,"http://nyi.nyi")
+    h = House(id=1,address="http://nyi.nyi")
     assert h.to_dict() == {'id': 1, 'address': "http://nyi.nyi"}
 
-def test_create_post():
-    nuke_db("sqlite:///test-openparlour-db.sqlite")
-    db = initialize_orm("sqlite:///test-openparlour-db.sqlite")
-    assert create_post(db, 1, "2020-12-01", "Test Title", "Test post body yeah yeah yeah!") == {'id': 1}
+
+def test_create_post(test_db):
+    assert create_post(test_db, room=1, date="2020-12-01", title="Test Title", body="Test post body yeah yeah yeah!") == 1
 
 
+def test_create_house(test_db):
+    assert create_house(test_db, address="https://nyi.nyi") == 1
